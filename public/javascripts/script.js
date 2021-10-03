941a94447b8d5f4e1d56a27a4b60262354154484
Sofia Mehta <sofiamehtaaa@gmail.com>
2021-10-03 18:33:48 -0400

    initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,43 @@
+/* File name - script.js, 
+            Student’s Name - Sofia Mehta, 
+            StudentID - 301171210, 
+            Date  3rd October 2021*/
+
+
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
