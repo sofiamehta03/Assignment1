427f7bdfe2713bc20900c1b9ca393ffa7b23b5fc
Sofia Mehta <sofiamehtaaa@gmail.com>
2021-10-03 20:57:31 -0400

    commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var usersRouter = require('./routes/users');
 
 
 var app = express();
-const port = 3000;
+// const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,9 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
